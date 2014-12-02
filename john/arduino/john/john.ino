c3d2dfb94519f67ec56ee6c3f8583e2359a281a7
Nicco Kunzmann <niccokunzmann@rambler.ru>
2014-12-02 10:37:23 +0100

    arduino code für John

---
+++ john/arduino/john/john.ino
@@ -0,0 +1,26 @@
+
+
+int pin_enable = 2;
+int pin_pwm_1 = 11;
+int pin_pwm_2 = 10;
+
+void setup() {
+  pinMode(pin_enable, OUTPUT);
+  pinMode(pin_pwm_1, OUTPUT);
+  pinMode(pin_pwm_2, OUTPUT);
+  
+  digitalWrite(pin_enable, HIGH); // anschalten
+  
+}
+
+void loop () {
+  digitalWrite(pin_pwm_1, LOW);
+  delay(500);
+  digitalWrite(pin_pwm_2, HIGH);
+  delay(1500); // fahren
+  digitalWrite(pin_pwm_2, LOW);
+  delay(500);
+  digitalWrite(pin_pwm_1, HIGH);
+  delay(1500); // fahren
+  
+}
