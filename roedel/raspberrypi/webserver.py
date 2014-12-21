1f5ce8f66e67fa44d7e64206fafdd38957e611a8
Nicco Kunzmann <niccokunzmann@aol.com>
2014-12-21 17:55:12 +0000

    webserver geht erstmal

--- roedel/raspberrypi/webserver.py
+++ roedel/raspberrypi/webserver.py
@@ -10,8 +10,8 @@ import time
 
 pin = 23
 
-HIGH = False
-LOW = True
+HIGH = True
+LOW = False
 
 # Pulse information for Modelcraft RS2 Servo
 #   http://www.servodatabase.com/servo/modelcraft/rs-2
@@ -19,9 +19,6 @@ LOW = True
 PULSE_CYCLE = 0.02 # seconds
 PULSE_WIDTH_MIN = 0.00054 # seconds
 PULSE_WIDTH_MAX = 0.00247 # seconds
-
-PULSE_WIDTH_MIN = 0.001 # seconds
-PULSE_WIDTH_MAX = 0.002 # seconds
 ROTATIONAL_RANGE = 203 # degrees
 
 
@@ -32,22 +29,17 @@ if IS_ON_RASPBERRY_PI:
     GPIO.setup(pin, GPIO.OUT)
     def set_servo_position(degrees):
         global last_servo_pulse
-        #print("set servo position to {}".format(degrees))
-        #now = time.time()
-        #if last_servo_pulse + PULSE_CYCLE > now:
-        #    time.sleep(last_servo_pulse + PULSE_CYCLE - now)
-        #last_servo_pulse = now
+        now = time.time()
+        if last_servo_pulse + PULSE_CYCLE > now:
+            time.sleep(last_servo_pulse + PULSE_CYCLE - now)
+        last_servo_pulse = time.time()
         degrees = degrees % 360
         if degrees > ROTATIONAL_RANGE:
             degrees = ROTATIONAL_RANGE
         pulse_width = PULSE_WIDTH_MIN + (PULSE_WIDTH_MAX - PULSE_WIDTH_MIN) * degrees / ROTATIONAL_RANGE
-        a = time.time()
         GPIO.output(pin, HIGH)
         time.sleep(pulse_width)
-        b = time.time()
         GPIO.output(pin, LOW)
-        time.sleep(PULSE_CYCLE)
-        print("set servo position to {} {:.6f} {:.6f}".format(degrees, pulse_width, b - a - pulse_width))
 
 else:
     last_servo_position = None
@@ -59,7 +51,7 @@ else:
         time.sleep(0.02)
         
 
-expected_servo_value = 0
+expected_servo_value = ROTATIONAL_RANGE / 2
 
 def set_the_servo_value_loop():
     global set_the_servo_value_thread_is_started
@@ -73,7 +65,7 @@ def set_the_servo_value_loop():
 set_the_servo_value_thread_is_started = False
 set_the_servo_value_thread = threading.Thread(target = set_the_servo_value_loop)
 set_the_servo_value_thread.deamon = True
-#set_the_servo_value_thread.start()
+set_the_servo_value_thread.start()
 
 
 app = Bottle()
@@ -86,11 +78,13 @@ def servo_position(degrees):
         set_servo_position(degrees)
     return "Setting servo position to {}°.".format(int(degrees))
 
-# run(app, host='', port=8080)
+run(app, host='', port=8080)
 
-while 1:
+while 0:
+    print(0)
     for i in range(100):
         set_servo_position(0)
+    print(100)
     for i in range(100):
         set_servo_position(100)
     
