3a96df7d1dee8cf088e2789c425a33e374066181
Nicco Kunzmann <niccokunzmann@rambler.ru>
2015-02-17 22:53:45 +0000

    made configurationload and save

--- roedel/raspberrypi/robot/configuration.py
+++ roedel/raspberrypi/robot/configuration.py
@@ -4,35 +4,60 @@ def local_path(path):
     here = os.path.dirname(__file__)
     return os.path.join(here, path)
 
-class WEBSERVER:
+CONFIGURATION_FILE = 'configuration.json'
 
-    PORT = 8080
-    HOST = ''
-    BLOCKLY_INDEX_URL = 'https://rawgit.com/niccokunzmann/blockly/master/demos/robots/index.html'
-    BLOCKLY_INDEX_PATH = 'demos/robots/index.html'
-    BLOCKLY_LOCAL_DIRECTORY = local_path('blockly')
-    REGISTER_SERVER_URL = 'http://rustyrobots.pythonanywhere.com/new_robot'
-    ROBOTER_IMAGE_URL = 'https://raw.githubusercontent.com/CoderDojoPotsdam/material/master/roboter/roedelmitraspberrypi/roedelmitraspberrypi.jpg'
-    BROADCAST_PORT = 5458
+class ConfigurationEntry:
+    def __init__(self, dict):
+        self.__dict__ = dict
 
-class SERVOSERVER:
-    
-    PORT = 8081
-    HOST = '127.0.0.1'
+WEBSERVER = ConfigurationEntry(dict(
+    PORT = 8080,
+    HOST = '',
+    BLOCKLY_INDEX_URL = 'https://rawgit.com/niccokunzmann/blockly/master/demos/robots/index.html',
+    BLOCKLY_INDEX_PATH = 'demos/robots/index.html',
+    BLOCKLY_LOCAL_DIRECTORY = local_path('blockly'),
+    REGISTER_SERVER_URL = 'http://rustyrobots.pythonanywhere.com/new_robot',
+    ROBOTER_IMAGE_URL = 'https://raw.githubusercontent.com/CoderDojoPotsdam/material/master/roboter/roedelmitraspberrypi/roedelmitraspberrypi.jpg',
+    BROADCAST_PORT = 5458,
+))
 
-class SERVO:
+SERVOSERVER = ConfigurationEntry(dict(    
+    PORT = 8081,
+    HOST = '127.0.0.1',
+))
 
+SERVO = ConfigurationEntry(dict(    
     # RPIO uses GPIO pin labeling as default
-    PIN = 11
+    PIN = 11,
 
     # Pulse information for Modelcraft RS2 Servo
     #   http://www.servodatabase.com/servo/modelcraft/rs-2
-    PULSE_CYCLE = 0.02 # seconds
-    PULSE_WIDTH_MIN = 0.00054 # seconds
-    PULSE_WIDTH_MAX = 0.00247 # seconds
-    ROTATIONAL_RANGE = 203 # degrees
+    PULSE_CYCLE = 0.02, # seconds
+    PULSE_WIDTH_MIN = 0.00054, # seconds
+    PULSE_WIDTH_MAX = 0.00247, # seconds
+    ROTATIONAL_RANGE = 203, # degrees
     # adjusment time according to
     #   http://www.servodatabase.com/servo/modelcraft/rs-2
     # 0.19 sec/60°
     # ROTATIONAL_RANGE / 60 * 0.19 # seconds
-    MOVEMENT_SPEED_IN_SECONDS_PER_DEGREES = 0.19 / 60
+    MOVEMENT_SPEED_IN_SECONDS_PER_DEGREES = 0.19 / 60,
+))
+
+configuration = dict(
+    WEBSERVER = WEBSERVER.__dict__,
+    SERVOSERVER = SERVOSERVER.__dict__,
+    SERVO = SERVO.__dict__,
+)
+
+def load():
+    if os.path.isfile(CONFIGURATION_FILE):
+        with open(configuration_file()) as f:
+            configuration = json.load(f)
+            for key, value in configuration:
+                globals()[key].__dict__ = value
+
+def dump():
+    with open(CONFIGURATION_FILE, "w") as f:
+        json.dump(configuration, f, sort_keys=True, indent = 4)
+
+load()
