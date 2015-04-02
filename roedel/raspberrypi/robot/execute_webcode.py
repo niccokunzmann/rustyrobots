550f468fdf0092f734c93d35b3451dbe7c12b96f
Nicco Kunzmann <niccokunzmann@rambler.ru>
2015-04-02 12:29:10 +0200

    refactored sevgo server and webserver

--- roedel/raspberrypi/robot/execute_webcode.py
+++ roedel/raspberrypi/robot/execute_webcode.py
@@ -1,42 +1,10 @@
 #!/usr/bin/python3
-import servo_control
 import time
 import sys
 import threading
-import urllib.request
-
-last_degrees = None
-lock = threading.Lock()
-
-def _set_servo_position(degrees):
-    # direct usage of the servo_control will remove the ability of
-    # the server to control the servo
-    url = 'http://localhost:{}/servo_position/{}'.format(PORT, degrees)
-    r = urllib.request.urlopen(url)
-    r.read()
-    r.close()
-
-def sleep_until_in_position(degrees):
-    global last_degrees
-    # adjusment time according to
-    #   http://www.servodatabase.com/servo/modelcraft/rs-2
-    # 0.19 sec/60°
-    if last_degrees is None:
-        degree_difference = servo_control.ROTATIONAL_RANGE
-    else:
-        degree_difference = abs(degrees - last_degrees)
-    time_for_rotation = 0.19 * degree_difference / 60
-    time.sleep(time_for_rotation)
-    last_degrees = degrees
-
-def set_servo_position(degrees = 0):
-    with lock:
-        _set_servo_position(degrees)
-        sleep_until_in_position(degrees)
-    
+from servo_client import *
 
 code = sys.argv[1]
-PORT = sys.argv[2]
 
 _print = print
 def print(*args, **kw):
@@ -55,5 +23,4 @@ thread.daemon = True
 thread.start()
 
 input()
-lock.acquire() # do not run into GPIO code when exiting
 
