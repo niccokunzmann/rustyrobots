024ad6ab0234071b3e393c5abe03476a00b8cc64
Nicco Kunzmann <niccokunzmann@rambler.ru>
2015-04-07 22:05:36 +0200

    added github lin

--- roedel/raspberrypi/internet/static/defaults.js
+++ roedel/raspberrypi/internet/static/defaults.js
@@ -5,4 +5,4 @@ HEADING_TEMPLATE = '\
     \
       <a href="index.html{server}"><h1 class="heading" id="index_heading">Rusty Robots</h1></a>\
       <a href="configuration.html{server}"><h1 class="heading" id="configuration_heading">Configuration</h1></a>\
-    ';
+      <a href="https://github.com/niccokunzmann/rustyrobots"><img style="position:absolute; top:0; right:0; border:0;" src="forkme_right_white_ffffff.png" alt="Fork me on GitHub"></a>';
