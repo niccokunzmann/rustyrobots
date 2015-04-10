afcb6e046ba72b047b295b39d65b0497e4f564bc
Nicco Kunzmann <niccokunzmann@googlemail.com>
2015-04-10 14:35:37 +0200

    Update download_and_install.sh

--- roedel/raspberrypi/robot/download_and_install.sh
+++ roedel/raspberrypi/robot/download_and_install.sh
@@ -1,1 +1,1 @@
-( cd ~ && ( sudo apt-get -y install git ; sudo rm -rf rustyrobots ; git clone http://github.com/niccokunzmann/rustyrobots.git --depth 1 ; cd rustyrobots/roedel/raspberrypi/robot && ./install.sh ))
+( cd ~ && ( sudo apt-get -y install git ; sudo rm -rf rustyrobots ; git clone http://github.com/niccokunzmann/rustyrobots.git --depth 1 ; cd rustyrobots/roedel/raspberrypi/robot && sudo ./install.sh ))
